9fcbbf254275da10c7c50b5f7840ffded5284433
Pedro Teixeira <i@pgte.me>
2018-06-21 09:10:49 +0100

    test for adding another pusher

--- test/collaboration-protocol.spec.js
+++ test/collaboration-protocol.spec.js
@@ -18,19 +18,21 @@ const Protocol = require('../src/collaboration/protocol')
 describe('collaboration protocol', function () {
   let pusher = {}
   let puller = {}
+  let pusher2 = {}
 
   it('pusher can be created', () => {
     const ipfs = {
       id () {
         return { id: 'pusher' }
       },
+      _peerInfo: fakePeerInfoFor('pusher'),
       _repo: {
         datastore: new MemoryDatastore()
       }
     }
     const collaboration = { name: 'collaboration protocol test' }
     pusher.store = new Store(ipfs, collaboration)
-    pusher.protocol = Protocol(collaboration, pusher.store)
+    pusher.protocol = Protocol(ipfs, collaboration, pusher.store)
     return pusher.store.start()
   })
 
@@ -39,13 +41,14 @@ describe('collaboration protocol', function () {
       id () {
         return { id: 'puller' }
       },
+      _peerInfo: fakePeerInfoFor('puller'),
       _repo: {
         datastore: new MemoryDatastore()
       }
     }
     const collaboration = { name: 'collaboration protocol test' }
     puller.store = new Store(ipfs, collaboration)
-    puller.protocol = Protocol(collaboration, puller.store)
+    puller.protocol = Protocol(ipfs, collaboration, puller.store)
     return puller.store.start()
   })
 
@@ -57,7 +60,7 @@ describe('collaboration protocol', function () {
       source: p1.source,
       sink: p2.sink,
       getPeerInfo (cb) {
-        setImmediate(() => cb(null, 'pusher'))
+        setImmediate(() => cb(null, fakePeerInfoFor('pusher')))
       }
     }
 
@@ -66,11 +69,11 @@ describe('collaboration protocol', function () {
       sink: p1.sink
     }
 
-    pusher.protocol.dialerFor('puller', pusherStream)
+    pusher.protocol.dialerFor(fakePeerInfoFor('puller'), pusherStream)
     puller.protocol.handler(null, pullerStream)
   })
 
-  it('can push operations', () => {
+  it('can save state locally', () => {
     return pusher.store.saveState([undefined, 'state 1'])
   })
 
@@ -79,4 +82,63 @@ describe('collaboration protocol', function () {
       expect(state).to.equal('state 1')
     })
   })
+
+  it('introduces another pusher', () => {
+    const ipfs = {
+      id () {
+        return { id: 'pusher 2' }
+      },
+      _peerInfo: fakePeerInfoFor('pusher 2'),
+      _repo: {
+        datastore: new MemoryDatastore()
+      }
+    }
+    const collaboration = { name: 'collaboration protocol test' }
+    pusher2.store = new Store(ipfs, collaboration)
+    pusher2.protocol = Protocol(ipfs, collaboration, pusher2.store)
+    return pusher2.store.start()
+  })
+
+  it('connects new pusher to puller', () => {
+    const p1 = pair()
+    const p2 = pair()
+
+    const pullerStream = {
+      source: p1.source,
+      sink: p2.sink,
+      getPeerInfo (cb) {
+        setImmediate(() => cb(null, fakePeerInfoFor('pusher 2')))
+      }
+    }
+
+    const pusherStream = {
+      source: p2.source,
+      sink: p1.sink
+    }
+
+    pusher2.protocol.dialerFor(fakePeerInfoFor('puller 2'), pusherStream)
+    puller.protocol.handler(null, pullerStream)
+  })
+
+  it('pusher2 can save state locally', () => {
+    return pusher2.store.saveState([undefined, 'state 2'])
+  })
+
+  it('waits a bit', (done) => setTimeout(done, 1000))
+
+  it('puller got new state', () => {
+    return puller.store.getState().then((state) => {
+      expect(state).to.equal('state 2')
+    })
+  })
 })
+
+function fakePeerInfoFor (id) {
+  return {
+    id: {
+      toB58String() {
+        return id
+      }
+    }
+  }
+}
