c3591b00deb8c789cc9a84c10ec2dbecde306603
Pedro Teixeira <i@pgte.me>
2018-07-18 09:54:16 +0100

    WIP: preparing for the official version

--- src/transport/ipfs.js
+++ src/transport/ipfs.js
@@ -3,6 +3,7 @@
 const IPFS = require('ipfs')
 const WebSocketStar = require('libp2p-websocket-star')
 const AppTransport = require('./app-transport')
+const Discovery = require('./discovery')
 
 module.exports = (app, options) => {
   const ipfs = new IPFS({
@@ -23,18 +24,35 @@ module.exports = (app, options) => {
   return ipfs
 
   function modules (peerInfo) {
-    const appTransport = AppTransport(app, ipfs, new WebSocketStar({ id: peerInfo.id }), options && options.transport)
-    appTransport.on('error', (err) => app.emit('error', err))
-    appTransport.on('peer connected', (peerInfo) => app.emit('peer connected', peerInfo))
-    appTransport.on('outbound peer connected', (peerInfo) => app.emit('outbound peer connected', peerInfo))
-    appTransport.on('inbound peer connected', (peerInfo) => app.emit('inbound peer connected', peerInfo))
-    appTransport.on('peer disconnected', (peerInfo) => app.emit('peer disconnected', peerInfo))
-    appTransport.on('outbound peer disconnected', (peerInfo) => app.emit('outbound peer disconnected', peerInfo))
-    appTransport.on('inbound peer disconnected', (peerInfo) => app.emit('inbound peer disconnected', peerInfo))
+    let appTransport
+
+    const T = class extends AppTransport {
+      constructor () {
+        super(app, ipfs, new WebSocketStar({ id: peerInfo.id }), options && options.transport)
+        // super hack
+        appTransport = this
+
+        appTransport.on('error', (err) => app.emit('error', err))
+        appTransport.on('peer connected', (peerInfo) => app.emit('peer connected', peerInfo))
+        appTransport.on('outbound peer connected', (peerInfo) => app.emit('outbound peer connected', peerInfo))
+        appTransport.on('inbound peer connected', (peerInfo) => app.emit('inbound peer connected', peerInfo))
+        appTransport.on('peer disconnected', (peerInfo) => app.emit('peer disconnected', peerInfo))
+        appTransport.on('outbound peer disconnected', (peerInfo) => app.emit('outbound peer disconnected', peerInfo))
+        appTransport.on('inbound peer disconnected', (peerInfo) => app.emit('inbound peer disconnected', peerInfo))
+      }
+    }
+
+    const D = class extends Discovery {
+      constructor () {
+        // super hack
+        console.log('discovery:', appTransport.discovery)
+        Object.assign(this, appTransport.discovery)
+      }
+    }
 
     return {
-      transport: [ appTransport ],
-      discovery: [ appTransport.discovery ]
+      transport: [ T ],
+      discovery: [ D ]
     }
   }
 }
