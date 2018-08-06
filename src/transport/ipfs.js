52b65ea9be008f98bd29524c7899b501ec60c042
Pedro Teixeira <i@pgte.me>
2018-08-06 11:58:30 +0100

    add libp2p-bootstrap as discovery method

--- src/transport/ipfs.js
+++ src/transport/ipfs.js
@@ -3,6 +3,7 @@
 const IPFS = require('ipfs')
 const WebSocketStar = require('libp2p-websocket-star')
 const WebSockets = require('libp2p-websockets')
+const Bootstrap = require('libp2p-bootstrap')
 const AppTransport = require('./app-transport')
 const Relay = require('./ipfs-relay')
 
@@ -16,19 +17,25 @@ module.exports = (app, options) => {
       Addresses: {
         Swarm: (options && options.swarm) || ['/dns4/ws-star1.par.dwebops.pub/tcp/443/wss/p2p-websocket-star']
       },
-      Bootstrap: options.bootstrap || []
-      // Bootstrap: [
-      //   '/dns4/ams-1.bootstrap.libp2p.io/tcp/443/wss/ipfs/QmSoLer265NRgSp2LA3dPaeykiS1J6DifTC88f5uVQKNAd',
-      //   '/dns4/lon-1.bootstrap.libp2p.io/tcp/443/wss/ipfs/QmSoLMeWqB7YGVLJN3pNLQpmmEk35v6wYtsMGLzSr5QBU3',
-      //   '/dns4/sfo-3.bootstrap.libp2p.io/tcp/443/wss/ipfs/QmSoLPppuBtQSGwKDZT2M73ULpjvfd3aZ6ha4oFGL1KrGM',
-      //   '/dns4/sgp-1.bootstrap.libp2p.io/tcp/443/wss/ipfs/QmSoLSafTMBsPKadTEgaXctDQVcqN88CNLHXMkTNwMKPnu',
-      //   '/dns4/nyc-1.bootstrap.libp2p.io/tcp/443/wss/ipfs/QmSoLueR4xBeUbY9WZ9xGUUxunbKWcrNFTDAadQJmocnWm',
-      //   '/dns4/nyc-2.bootstrap.libp2p.io/tcp/443/wss/ipfs/QmSoLV4Bbm51jM9C4gDYZQ9Cy3U6aXMJDAbzgu2fzaDs64',
-      //   '/dns4/node0.preload.ipfs.io/tcp/443/wss/ipfs/QmZMxNdpMkewiVZLMRxaNxUeZpDUb34pWjZ1kZvsd16Zic',
-      //   '/dns4/node1.preload.ipfs.io/tcp/443/wss/ipfs/Qmbut9Ywz9YEDrz8ySBSgWyJk41Uvm2QJPhwDJzJyGFsD6'
-      // ]
+      Bootstrap: options.bootstrap
     },
-    libp2p: { modules }
+    libp2p: {
+      modules,
+      config: {
+        peerDiscovery: {
+          bootstrap: {
+            enabled: true
+          },
+          websocketStar: {
+            enabled: true
+          }
+        }
+      }
+    }
+  }
+
+  if (options.bootstrap) {
+    ipfsOptions.config.Bootstrap = options.bootstrap
   }
 
   if (options.relay) {
@@ -39,6 +46,7 @@ module.exports = (app, options) => {
       }
     }
   }
+
   const ipfs = new IPFS(ipfsOptions)
 
   return ipfs
@@ -59,7 +67,7 @@ module.exports = (app, options) => {
 
     return {
       transport: [ appTransport, WebSockets ],
-      peerDiscovery: [ appTransport.discovery ]
+      peerDiscovery: [ appTransport.discovery, Bootstrap ]
     }
   }
 }
