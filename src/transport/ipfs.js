c51f2376c6e802de434f8e5eb9adabd79327e826
Alan Shaw <alan@tableflip.io>
2018-09-11 13:10:57 +0100

    chore: update to IPFS 0.32
    Includes changes to create a libp2p node from a function.
    
    Tests all passing locally for me \o/
    
    License: MIT
    Signed-off-by: Alan Shaw <alan@tableflip.io>

--- src/transport/ipfs.js
+++ src/transport/ipfs.js
@@ -1,9 +1,13 @@
 'use strict'
 
 const IPFS = require('ipfs')
+const Libp2p = require('libp2p')
 const WebSocketStar = require('libp2p-websocket-star')
 const WebSockets = require('libp2p-websockets')
 const Bootstrap = require('libp2p-bootstrap')
+const Multiplex = require('libp2p-mplex')
+const SECIO = require('libp2p-secio')
+const get = require('lodash/get')
 const AppTransport = require('./app-transport')
 const Relay = require('./ipfs-relay')
 
@@ -19,19 +23,7 @@ module.exports = (app, options) => {
       },
       Bootstrap: options.bootstrap
     },
-    libp2p: {
-      modules,
-      config: {
-        peerDiscovery: {
-          bootstrap: {
-            enabled: true
-          },
-          websocketStar: {
-            enabled: true
-          }
-        }
-      }
-    }
+    libp2p: createLibp2p
   }
 
   if (options.bootstrap) {
@@ -51,7 +43,12 @@ module.exports = (app, options) => {
 
   return ipfs
 
-  function modules (peerInfo) {
+  function createLibp2p ({
+    config,
+    options: ipfsOptions,
+    peerInfo,
+    peerBook
+  }) {
     const appTransport = AppTransport(app, ipfs, new WebSocketStar({ id: peerInfo.id }), options && options.transport)
     appTransport.on('error', (err) => app.emit('error', err))
     appTransport.on('peer connected', (peerInfo) => app.emit('peer connected', peerInfo))
@@ -65,9 +62,40 @@ module.exports = (app, options) => {
       Relay(ipfs, appTransport, options.relay)
     }
 
-    return {
-      transport: [ appTransport, WebSockets ],
-      peerDiscovery: [ appTransport.discovery, Bootstrap ]
-    }
+    return new Libp2p({
+      peerInfo,
+      peerBook,
+      modules: {
+        transport: [ appTransport, WebSockets ],
+        streamMuxer: [ Multiplex ],
+        connEncryption: [ SECIO ],
+        peerDiscovery: [ appTransport.discovery, Bootstrap ]
+      },
+      config: {
+        peerDiscovery: {
+          bootstrap: {
+            list: get(ipfsOptions, 'config.Bootstrap',
+              get(config, 'Bootstrap', []))
+          },
+          websocketStar: {
+            enabled: true
+          }
+        },
+        relay: {
+          enabled: get(ipfsOptions, 'relay.enabled',
+            get(config, 'relay.enabled', false)),
+          hop: {
+            enabled: get(ipfsOptions, 'relay.hop.enabled',
+              get(config, 'relay.hop.enabled', false)),
+            active: get(ipfsOptions, 'relay.hop.active',
+              get(config, 'relay.hop.active', false))
+          }
+        },
+        EXPERIMENTAL: {
+          dht: get(ipfsOptions, 'EXPERIMENTAL.dht', false),
+          pubsub: get(ipfsOptions, 'EXPERIMENTAL.pubsub', false)
+        }
+      }
+    })
   }
 }
