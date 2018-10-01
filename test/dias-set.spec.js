2ed4072bfc5d1583fadfe66374924b92866d7e48
Jim Pick <jim@backblend.com>
2018-10-01 14:40:44 -0700

    Add fix for dias set
    Need to make a simpler test case.

--- test/dias-set.spec.js
+++ test/dias-set.spec.js
@@ -7,6 +7,8 @@ const expect = chai.expect
 
 const Ring = require('../src/common/ring')
 const DiasSet = require('../src/common/dias-peer-set')
+const PeerId = require('peer-id')
+const PeerInfo = require('peer-info')
 
 describe('dias set', () => {
   const id = [0, 0]
@@ -64,6 +66,44 @@ describe('dias set', () => {
     expect(
       Array.from(diasSet(r).values()).map(peerInfoToId).sort(sort))
       .to.deep.equal([[0, 1], [0, 2], [51, 0], [63, 0], [85, 0], [128, 0]])
+  })
+
+  it('can work with poorly distributed small sets', () => {
+    r = Ring(2)
+    const peerInfo = new PeerInfo(PeerId.createFromB58String('QmQbmmST1FUR3yV6duqVanpipu7CvR5cQJkVbWAesQHZoA'))
+    diasSet = DiasSet(32, peerInfo, 2)
+    r.add(new PeerInfo(PeerId.createFromB58String('QmTbtfVDtQcQRsw8qTCxaKC2e9A73vhTkY96Vd78jhEUpE')))
+    r.add(new PeerInfo(PeerId.createFromB58String('QmeT1DABf1S1h3fUS9AT8FXZDYibAwoCg2yMgF2LQTdNYT')))
+    r.add(new PeerInfo(PeerId.createFromB58String('QmXk1e4WjpU8zkELePbfM7y2piH7hMZhpyrFkPzWBuhsdN')))
+    r.add(new PeerInfo(PeerId.createFromB58String('QmehDvwCWhcHSvFWKit59Liuxxu28N17Rm5pdpPN6uFC5H')))
+    r.add(new PeerInfo(PeerId.createFromB58String('QmY2VneWoQW9KgjhfC6FQJxMtpL6NwLKmnreahgmSvJjM2')))
+    r.add(new PeerInfo(PeerId.createFromB58String('QmeRizpk55kPQJ1T5kfqLHBzRFoocD3BDDbQMakRdNskst')))
+    console.log('Jim ring')
+    r._points.forEach(point => {
+      console.log(
+        '  ', point.toString('hex'),
+        r._contacts.get(point.toString('hex')).id.toB58String()
+      )
+    })
+    console.log('Jim ring from', peerInfo.id.toB58String())
+    let cursor = r.successorOf(peerInfo)
+    console.log('Jim ring0', cursor.id.toB58String())
+    cursor = r.successorOf(cursor)
+    console.log('Jim ring1', cursor.id.toB58String())
+    cursor = r.successorOf(cursor)
+    console.log('Jim ring2', cursor.id.toB58String())
+    cursor = r.successorOf(cursor)
+    console.log('Jim ring3', cursor.id.toB58String())
+    cursor = r.successorOf(cursor)
+    console.log('Jim ring4', cursor.id.toB58String())
+    cursor = r.successorOf(cursor)
+    console.log('Jim ring5', cursor.id.toB58String())
+    cursor = r.successorOf(cursor)
+    console.log('Jim ring6', cursor.id.toB58String())
+    console.log('Jim',
+      [...r._contacts.keys()].map(key => [key, r._contacts.get(key).id.toB58String().slice(-3)]),
+      Array.from(diasSet(r).values()).map(peerInfoToId).sort(sort)
+    )
   })
 })
 
