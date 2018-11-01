d5f01dc8eb39ca38f99bdfe014c6f1e2b444dceb
Pedro Teixeira <pedro@protocol.ai>
2018-11-01 22:04:00 +0000

    fix: DiasSet break points (#101)

--- src/common/dias-peer-set.js
+++ src/common/dias-peer-set.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const PeerSet = require('./peer-set')
+const BigInt = require('big-integer')
 
 module.exports = (bytes, peerInfo, preambleBytes) => {
   const id = peerInfo.id.toBytes().slice(preambleBytes)
@@ -11,7 +12,7 @@ module.exports = (bytes, peerInfo, preambleBytes) => {
   const oneFifth = sum(id, divideBy(fullRing, 5))
   const oneFourth = sum(id, divideBy(fullRing, 4))
   const oneThird = sum(id, divideBy(fullRing, 3))
-  const oneHalf = oneHalfFrom(id)
+  const oneHalf = sum(id, oneHalfFrom(fullRing))
 
   return (ring) => {
     const peers = new PeerSet()
@@ -40,47 +41,42 @@ module.exports = (bytes, peerInfo, preambleBytes) => {
       if (!peers.has(peer)) {
         peers.add(peer)
       } else {
-        if (peer === stop) return
+        if (peer === stop) return;
         addOrAddSuccessor(ring.successorOf(peer), stop || peer)
       }
     }
   }
 
-  function sum (a, b) {
-    const result = Buffer.alloc(bytes)
-    let carry = 0
-    for (let i = bytes - 1; i >= 0; i--) {
-      const l = a[i] || 0
-      const r = b[i] || 0
-      const s = l + r + carry
-      result[i] = s & 0xff
-      carry = s >> 8
-    }
+  function sum (_a, _b) {
+    const a = numberFromId(_a)
+    const b = numberFromId(_b)
+    const result = a.add(b).mod(numberFromId(fullRing))
+    return idFromNumber(result)
+  }
 
+  function divideBy (_id, d) {
+    const result = numberFromId(_id).divide(d)
+    return idFromNumber(result)
+  }
+
+  function numberFromId (id) {
+    const str = id.toString('hex')
+    const result = BigInt(str, 16)
     return result
   }
 
-  function divideBy (id, d) {
-    const result = Buffer.alloc(bytes)
-    let remainder = 0
-    for (let i = 0; i < bytes; i++) {
-      let byte = id[i] + (remainder << 8)
-      if (byte < d) {
-        result[i] = 0
-        byte = (byte << 8) + id[++i]
-      }
-      result[i] = Math.floor(byte / d)
-      remainder = byte % d
+  function idFromNumber (i) {
+    const resultBytes = i.toArray(256).value
+    while (resultBytes.length < bytes) {
+      resultBytes.unshift(0)
     }
-    return result
+
+    return Buffer.from(resultBytes)
   }
 
   function oneHalfFrom (id) {
-    const half = Buffer.alloc(bytes)
-    for (let i = 1; i < bytes; i++) {
-      half[i] = 0xff
-    }
-    half[0] = 0x7f
-    return sum(bytes, half)
+    const n = numberFromId(id)
+    const result = n.divide(2)
+    return idFromNumber(result)
   }
 }
