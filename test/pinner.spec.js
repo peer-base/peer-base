a864d29619807dabd0f6f9c9575b0fd89d6eeae0
Pedro Teixeira <i@pgte.me>
2018-11-06 11:32:23 +0000

    chore: pinner tests

---
+++ test/pinner.spec.js
@@ -0,0 +1,115 @@
+/* eslint-env mocha */
+'use strict'
+
+const chai = require('chai')
+chai.use(require('dirty-chai'))
+const expect = chai.expect
+
+const PeerStar = require('../')
+const App = require('./utils/create-app')
+const A_BIT = 10000
+
+describe('pinner', function () {
+  this.timeout(20000)
+
+  const collaborationName = 'pinner test collab'
+  const peerCount = 2 // 10
+  const collaborationOptions = {}
+
+  let swarm = []
+  let pinner
+  let collaborations
+  let newReader
+  let newReaderCollab
+  let expectedValue
+
+  for (let i = 0; i < peerCount; i++) {
+    ((i) => {
+      before(() => {
+        const app = App({ maxThrottleDelayMS: 1000 })
+        swarm.push(app)
+        return app.start()
+      })
+    })(i)
+  }
+
+  before(async () => {
+    collaborationOptions.keys = await PeerStar.keys.generate()
+  })
+
+  before((done) => {
+    // wait a bit for things to sync
+    setTimeout(done, A_BIT)
+  })
+
+  before(async () => {
+    collaborations = await Promise.all(
+      swarm.map((peer) => peer.app.collaborate(collaborationName, 'gset', collaborationOptions)))
+    expect(collaborations.length).to.equal(peerCount)
+  })
+
+  before((done) => {
+    setTimeout(done, A_BIT)
+  })
+
+  it('can add a pinner to a collaboration', () => {
+    pinner = PeerStar.createPinner(App.appName, {
+      ipfs: {
+        swarm: App.swarm
+      }
+    })
+    return pinner.start()
+  })
+
+  it('waits a bit', (done) => {
+    setTimeout(done, A_BIT)
+  })
+
+  it('peers can perform mutations', () => {
+    let pendingChanges = collaborations.length
+    collaborations.forEach((collaboration, idx) => {
+      collaboration.shared.add(idx)
+    })
+  })
+
+  it('waits a bit', (done) => {
+    setTimeout(done, A_BIT)
+  })
+
+  it('converges between replicas', () => {
+    expectedValue = new Set()
+    collaborations.forEach((collaboration, idx) => {
+      expectedValue.add(idx)
+    })
+    collaborations.forEach((collaboration) => {
+      expect(collaboration.shared.value()).to.deep.equal(expectedValue)
+    })
+  })
+
+  it('stops all replicas except for pinner', () => {
+    return Promise.all(swarm.map(peer => peer.stop()))
+  })
+
+  it('can start new reader', async () => {
+    newReader = App({ maxThrottleDelayMS: 1000 })
+    swarm.push(newReader)
+    await newReader.start()
+    newReaderCollab = await newReader.app.collaborate(collaborationName, 'gset', collaborationOptions)
+  })
+
+  it('waits a bit', (done) => {
+    setTimeout(done, A_BIT)
+  })
+
+  it('new reader got state', () => {
+    expect(newReaderCollab.shared.value()).to.deep.equal(expectedValue)
+  })
+
+  it('can stop new reader', () => {
+    return newReader.stop()
+  })
+
+  it('can stop new replica', () => {
+    return pinner.stop()
+  })
+})
--- test/utils/create-app.js
+++ test/utils/create-app.js
@@ -3,15 +3,18 @@
 const PeerStar = require('../../')
 const Repo = require('./repo')
 
+const APP_NAME = 'peer star test app'
+const SWARM = [ '/ip4/127.0.0.1/tcp/9090/ws/p2p-websocket-star' ]
+
 module.exports = (transportOptions, ipfsOptions) => {
   const repo = Repo()
 
   ipfsOptions = Object.assign({
     repo,
-    swarm: [ '/ip4/127.0.0.1/tcp/9090/ws/p2p-websocket-star' ]
+    swarm: SWARM
   }, ipfsOptions)
 
-  const app = PeerStar('peer star test app', {
+  const app = PeerStar(APP_NAME, {
     ipfs: ipfsOptions,
     transport: transportOptions
   })
@@ -26,3 +29,6 @@ module.exports = (transportOptions, ipfsOptions) => {
 
   return { app, start, stop }
 }
+
+module.exports.appName = APP_NAME
+module.exports.swarm = SWARM
