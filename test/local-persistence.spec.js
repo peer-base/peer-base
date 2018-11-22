c3e4037ab618203f6a9f5456e934578b2eea4661
Pedro Teixeira <i@pgte.me>
2018-11-22 18:39:00 +0000

    adds local persistence tests

---
+++ test/local-persistence.spec.js
@@ -0,0 +1,48 @@
+/* eslint-env mocha */
+'use strict'
+
+const chai = require('chai')
+chai.use(require('dirty-chai'))
+const expect = chai.expect
+
+const PeerStar = require('../')
+const App = require('./utils/create-app')
+const waitForMembers = require('./utils/wait-for-members')
+const waitForValue = require('./utils/wait-for-value')
+
+describe('local-persistence', function () {
+  this.timeout(30000)
+
+  let appName
+  let repo
+
+  before(() => {
+    appName = App.createName()
+  })
+
+  it('can create collaboration and populate it', async () => {
+    const app = App(appName, { maxThrottleDelayMS: 1000 })
+    await app.start()
+    repo = app.app.ipfs._repo
+    expect(repo).to.exist()
+
+    const collaboration = await app.app.collaborate('local persistence test collaboration', 'rga')
+    collaboration.on('saved', () => {
+      console.log('collaboration saved')
+    })
+    collaboration.shared.push('a')
+    collaboration.shared.push('b')
+    expect(collaboration.shared.value()).to.deep.equal(['a', 'b'])
+    await collaboration.stop()
+    await app.app.stop()
+  })
+
+  it('can revive collaboration from ipfs repo', async () => {
+    const app = App(appName, { maxThrottleDelayMS: 1000 }, { repo })
+    await app.start()
+    console.log('started app')
+    const collaboration = await app.app.collaborate('local persistence test collaboration', 'rga')
+    console.log('started collaboration')
+    expect(collaboration.shared.value()).to.deep.equal(['a', 'b'])
+  })
+})
