94a8cfb5ba94512b320015ca2d812b4fb5188049
Pedro Teixeira <i@pgte.me>
2018-11-24 17:02:38 +0000

    chore: collaboration deltas test: some more tests with random RGA appends

--- test/collaboration-deltas.spec.js
+++ test/collaboration-deltas.spec.js
@@ -5,6 +5,7 @@ const chai = require('chai')
 chai.use(require('dirty-chai'))
 const expect = chai.expect
 
+const delay = require('delay')
 const PeerStar = require('../')
 const App = require('./utils/create-app')
 const waitForMembers = require('./utils/wait-for-members')
@@ -15,6 +16,11 @@ describe('collaboration deltas', function () {
 
   const characters = ['a', 'b']
   const moreCharacters = ['A', 'B']
+  const evenMoreCharacters = ['å', '∫']
+  const yetMoreCharacters = ['Å', 'ß']
+
+  const manyCharacters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'.split('')
+
   const peerCount = characters.length
   const collaborationOptions = {}
 
@@ -55,19 +61,21 @@ describe('collaboration deltas', function () {
   it('can push concurrent operations', async () => {
     const deltaCounts = []
     const listeners = collaborations.map((collaboration, idx) => {
-      const onDelta = (delta) => {
-        const [added, removed, edges] = delta
-        expect(added.size).to.equal(2)
-        expect(added.has(null)).to.be.true()
-        expect(removed.size).to.equal(0)
-        expect(edges.size).to.equal(2)
-        expect(edges.has(null)).to.be.true()
-        deltaCounts[idx] = (deltaCounts[idx] || 0) + 1
+      const onDelta = (delta, fromSelf) => {
+        if (!fromSelf) {
+          const [added, removed, edges] = delta
+          expect(added.size).to.equal(2)
+          expect(removed.size).to.equal(0)
+          expect(edges.size).to.equal(2)
+          deltaCounts[idx] = (deltaCounts[idx] || 0) + 1
+        }
       }
       collaboration.shared.on('delta', onDelta)
       collaboration.shared.push(characters[idx])
       return onDelta
     })
+
+    await delay(3000)
 
     await waitForValue(collaborations, [...characters].reverse())
     collaborations.forEach((collaboration, idx) => {
@@ -75,33 +83,102 @@ describe('collaboration deltas', function () {
     })
 
     expect(deltaCounts.length).to.equal(peerCount)
-    deltaCounts.forEach((deltaCount) => expect(deltaCount).to.equal(2))
+    deltaCounts.forEach((deltaCount) => expect(deltaCount).to.equal(1))
   })
 
   it('can push more operations', async () => {
     const deltaCounts = []
     const listeners = collaborations.map((collaboration, idx) => {
-      const onDelta = (delta) => {
-        const [added, removed, edges] = delta
-        added.delete(null)
-        expect(added.size).to.equal(2)
-        expect(removed.size).to.equal(0)
-        expect(edges.size).to.equal(3)
-        expect(edges.has(null)).to.be.true()
-        deltaCounts[idx] = (deltaCounts[idx] || 0) + 1
+      const onDelta = (delta, fromSelf) => {
+        if (!fromSelf) {
+          const [added, removed, edges] = delta
+          expect(added.size).to.be.most(3)
+          expect(removed.size).to.equal(0)
+          expect(edges.size).to.be.most(3)
+          expect(edges.has(null)).to.be.true()
+          deltaCounts[idx] = (deltaCounts[idx] || 0) + 1
+        }
       }
       collaboration.shared.on('delta', onDelta)
       collaboration.shared.push(moreCharacters[idx])
       return onDelta
     })
 
+    await delay(3000)
     await waitForValue(collaborations, [...moreCharacters, ...characters].reverse())
+
     collaborations.forEach((collaboration, idx) => {
       collaboration.shared.removeListener('delta', listeners[idx])
     })
+
+    expect(deltaCounts.length).to.equal(peerCount)
+    deltaCounts.forEach((deltaCount) => expect(deltaCount).to.equal(1))
   })
 
-  it('closes peer', () => {
-    return swarm[swarm.length - 1].app.stop()
+  it('can diverge further', async () => {
+    const deltaCounts = []
+    const listeners = collaborations.map((collaboration, idx) => {
+      const onDelta = (delta, fromSelf) => {
+        if (!fromSelf) {
+          const [added, removed, edges] = delta
+          expect(added.size).to.be.most(4)
+          expect(removed.size).to.equal(0)
+          expect(edges.size).to.be.most(4)
+          expect(edges.has(null)).to.be.true()
+          deltaCounts[idx] = (deltaCounts[idx] || 0) + 1
+        }
+      }
+      collaboration.shared.on('delta', onDelta)
+      collaboration.shared.push(evenMoreCharacters[idx])
+      collaboration.shared.push(yetMoreCharacters[idx])
+      return onDelta
+    })
+
+    await delay(3000)
+    await waitForValue(collaborations, [...moreCharacters, ...characters].reverse().concat([evenMoreCharacters[1], yetMoreCharacters[1], evenMoreCharacters[0], yetMoreCharacters[0]]))
+
+    collaborations.forEach((collaboration, idx) => {
+      collaboration.shared.removeListener('delta', listeners[idx])
+    })
+
+    expect(deltaCounts.length).to.equal(peerCount)
+    deltaCounts.forEach((deltaCount) => expect(deltaCount).to.equal(1))
+  })
+
+  it('handles adding many more random changes', async () => {
+    let expectedCharacterCount = collaborations[0].shared.value().length
+    let expectedValue
+    const modifications = async (collaboration, index) => {
+      const characters = []
+      for (let i = 0; i < 100; i ++) {
+        const character = randomCharacter()
+        console.log('%d: pushing', index, character)
+        collaboration.shared.push(character)
+        expectedCharacterCount++
+        await delay(randomShortTime())
+      }
+
+      await delay(20000)
+
+      const value = collaboration.shared.value()
+      console.log('VALUE:', value)
+      if (!expectedValue) {
+        expectedValue = value
+      } else {
+        expect(value).to.deep.equal(expectedValue)
+      }
+    }
+
+    await Promise.all(collaborations.map(modifications))
+
+    expect(collaborations[0].shared.value().length).to.equal(expectedCharacterCount)
+
+    function randomShortTime () {
+      return Math.floor(Math.random() * 50)
+    }
+
+    function randomCharacter () {
+      return manyCharacters[Math.floor(Math.random() * manyCharacters.length)]
+    }
   })
 })
