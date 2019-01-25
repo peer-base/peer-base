3b2201e80e9a575f7e7a05b19e84418816b59cf9
Pedro Teixeira <i@pgte.me>
2019-01-25 09:02:51 +0000

    chore: some debugging

--- test/utils/wait-for-value.js
+++ test/utils/wait-for-value.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const isEqual = require('lodash/isEqual')
+const debug = require('debug')('peer-base:tests:waitforvalue')
 
 module.exports = (collaborations, value) => {
   if (!Array.isArray(collaborations)) {
@@ -14,13 +15,16 @@ function waitForCollaborationValue (collaboration, value) {
   return new Promise((resolve) => {
     const onStateChanged = () => {
       const currentValue = collaboration.shared.value()
+      debug('state changed. new value:', currentValue)
       if (isEqual(currentValue, value)) {
         collaboration.shared.removeListener('state changed', onStateChanged)
         setTimeout(resolve, 100)
       }
     }
     const currentValue = collaboration.shared.value()
+    debug('current value:', currentValue)
     if (isEqual(currentValue, value)) {
+      debug('done')
       resolve()
     } else {
       collaboration.shared.on('state changed', onStateChanged)
